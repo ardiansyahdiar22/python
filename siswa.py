8b5ea9b5297396252d59a80d2a0aa9b64850a9b4
diarardiansyah <ardiansyahdiar@gmail.com>
2022-01-05 19:52:10 +0700

    edit file siswa.py

--- siswa.py
+++ siswa.py
@@ -1,9 +1,11 @@
-print('====== Program sederhana with python ========')
+print('====== Program penilaian siswa ========')
 
 namaSiswa = input('Masukan nama siswa : ')
 nilai = float(input('Masukan nilai siswa : '))
 
 if nilai == 100:
-    print(f"Selamat {namaSiswa} kamu lulus dengan nilai sempurna A+")
+    print(f"Selamat {namaSiswa} kamu lulus dengan nilai grade A+")
 elif nilai <= 95 and nilai >= 85:
-    print(f"Selamat {namaSiswa} kamu lulus dengan predikat nilai A")
+    print(f"Selamat {namaSiswa} kamu lulus dengan grade A")
+elif nilai < 85 and nilai >= 75:
+    print(f"Selamat {namaSiswa} anda mendapatkan grade nilai B")
