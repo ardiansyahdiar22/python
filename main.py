040a4334c7ea4f28271ab65dc6b07824859394a3
diarardiansyah <ardiansyahdiar@gmail.com>
2022-01-05 19:27:39 +0700

    ädd new file main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+print('======= Program sederhana with python programming language ========')
+
+nama = input('Masukan nama anda : ')
+usia = int(input('Masukan usia anda : '))
+
+print(f"Halo {nama} saat ini kamu berusia {usia} Tahun")
